f589786790715646a54052dcd4f93c352542741f
Tijuan Brown <brownti159@gmail.com>
2021-04-21 17:55:23 -0400

    First animal for loop

---
+++ lyric.py
@@ -0,0 +1,16 @@
+def lyrics():
+    #animal_1 = "spider"
+    #animal_2 = "bird"
+    #animal_3 = "cat"
+    #animal_4 = "dog"
+    #animal_5 = "goat"
+    #animal_6 = "cow"
+    animals = ["spider", "bird", "cat", "dog", "goat", "cow"]
+    for animal in animals:
+        line = "I know an old lady who swallowed a %s. " % (animal)
+        print(line)
+
+lyrics()               
+    #while still going through the list of animals repeat "I don't know why she swallowed the fly. Perhaps she'll die."
+     
+    
